f40b85126b76badc7c739584a47b2b3c4ab3d1f9
tomas.mccandless <tomas.mccandless@workday.com>
2020-12-09 01:35:40 -0800

    day 6

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,9 @@ import System.IO
 import Lib
 import Prob1
 import Prob2
+import Prob4
+import Prob5
+import Prob6
 
 main :: IO ()
-main = prob2
+main = prob6
--- src/Util.hs
+++ src/Util.hs
@@ -1,7 +1,20 @@
-module Util (count, xor) where
+module Util (count, xor, takeRight, dropRight, quicksort) where
 
 count :: (a -> Bool) -> [a] -> Int
 count p xs = length (filter p xs)
 
 xor :: Bool -> Bool -> Bool
 xor a b = a /= b
+
+takeRight :: Int -> String -> String
+takeRight n = reverse . take n . reverse
+
+dropRight :: Int -> String -> String
+dropRight n = reverse . drop n . reverse
+
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
