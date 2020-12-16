27f9c985b64af7f94bd96b1fc42130eaa7cd110b
tomas.mccandless <tomas.mccandless@workday.com>
2020-12-16 14:53:12 -0800

    wip prob12

--- src/Prob12.hs
+++ src/Prob12.hs
@@ -5,14 +5,14 @@ import Data.String.Utils
 import System.IO
 import Util
 
-data Action = North | South | East | West | Left | Right | Forward
+data Action = MoveNorth | MoveSouth | MoveEast | MoveWest | TurnLeft | TurnRight | MoveForward
     deriving Show
-data Direction = FacingNorth | FacingSouth | FacingEast | FacingWest
+data FacingDirection = FacingNorth | FacingSouth | FacingEast | FacingWest
     deriving Show
 -- x, y
 type Position = (Int, Int)
 type Instruction = (Action, Int)
-type Ship = (Direction, Position)
+type Ship = (FacingDirection, Position)
 
 readData :: String -> IO [Instruction]
 readData fileName = do
@@ -27,28 +27,85 @@ readData fileName = do
     return instructions
 
 parseChar :: Char -> Action
-parseChar 'N' = North
-parseChar 'S' = South
-parseChar 'E' = East
-parseChar 'W' = West
-parseChar 'L' = Prob12.Left
-parseChar 'R' = Prob12.Right
-parseChar 'F' = Forward
+parseChar 'N' = MoveNorth
+parseChar 'S' = MoveSouth
+parseChar 'E' = MoveEast
+parseChar 'W' = MoveWest
+parseChar 'L' = TurnLeft
+parseChar 'R' = TurnRight
+parseChar 'F' = MoveForward
 parseChar other = error ("not supported: " ++ show other)
 
 parseLine :: String -> Instruction
 parseLine (cmd:arg) = (parseChar cmd, read arg :: Int)
 
-
+-- assume distance from origin
+manhattanDistance :: Int -> Int -> Int
+manhattanDistance x y = (abs x) + (abs y)
 
 runInstruction :: Ship -> Instruction -> Ship
-runInstruction (dir, (x, y)) (North, arg) = (dir, (x, y + arg))
-runInstruction (dir, (x, y)) (South, arg) = (dir, (x, y - arg))
-runInstruction (dir, (x, y)) (East, arg) = (dir, (x + arg, y))
-runInstruction (dir, (x, y)) (West, arg) = (dir, (x - arg, y))
-runInstruction ship _ = ship
--- TODO turning
+runInstruction (dir, (x, y)) (MoveNorth, arg) = (dir, (x, y + arg))
+runInstruction (dir, (x, y)) (MoveSouth, arg) = (dir, (x, y - arg))
+runInstruction (dir, (x, y)) (MoveEast, arg) = (dir, (x + arg, y))
+runInstruction (dir, (x, y)) (MoveWest, arg) = (dir, (x - arg, y))
+runInstruction (FacingNorth, (x, y)) (MoveForward, arg) = (FacingNorth, (x, y + arg))
+runInstruction (FacingSouth, (x, y)) (MoveForward, arg) = (FacingSouth, (x, y - arg))
+runInstruction (FacingEast, (x, y)) (MoveForward, arg) = (FacingEast, (x + arg, y))
+runInstruction (FacingWest, (x, y)) (MoveForward, arg) = (FacingWest, (x - arg, y))
+runInstruction (FacingNorth, pos) (TurnLeft, 90) = (FacingWest, pos)
+runInstruction (FacingNorth, pos) (TurnLeft, n) = runInstruction (FacingWest, pos) (TurnLeft, n - 90)
+runInstruction (FacingNorth, pos) (TurnRight, 90) = (FacingEast, pos)
+runInstruction (FacingNorth, pos) (TurnRight, n) = runInstruction (FacingEast, pos) (TurnRight, n - 90)
+runInstruction (FacingEast, pos) (TurnLeft, 90) = (FacingNorth, pos)
+runInstruction (FacingEast, pos) (TurnLeft, n) = runInstruction (FacingNorth, pos) (TurnLeft, n - 90)
+runInstruction (FacingEast, pos) (TurnRight, 90) = (FacingSouth, pos)
+runInstruction (FacingEast, pos) (TurnRight, n) = runInstruction (FacingSouth, pos) (TurnRight, n - 90)
+runInstruction (FacingSouth, pos) (TurnLeft, 90) = (FacingEast, pos)
+runInstruction (FacingSouth, pos) (TurnLeft, n) = runInstruction (FacingEast, pos) (TurnLeft, n - 90)
+runInstruction (FacingSouth, pos) (TurnRight, 90) = (FacingWest, pos)
+runInstruction (FacingSouth, pos) (TurnRight, n) = runInstruction (FacingWest, pos) (TurnRight, n - 90)
+runInstruction (FacingWest, pos) (TurnLeft, 90) = (FacingSouth, pos)
+runInstruction (FacingWest, pos) (TurnLeft, n) = runInstruction (FacingSouth, pos) (TurnLeft, n - 90)
+runInstruction (FacingWest, pos) (TurnRight, 90) = (FacingNorth, pos)
+runInstruction (FacingWest, pos) (TurnRight, n) = runInstruction (FacingNorth, pos) (TurnRight, n - 90)
 
+run :: Ship -> [Instruction] -> Ship
+run ship [] = ship
+run ship (i:is) = run (runInstruction ship i) is
+
+part1 :: [Instruction] -> Int
+part1 is =
+    let (_, (x, y))= run (FacingEast, (0, 0)) is
+        in manhattanDistance x y
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+-- relative to ship position
+type Waypoint = (Int, Int)
+
+-- we no longer care about ship facing direction
+runInstruction2 :: (Position, Waypoint) -> Instruction -> (Position, Waypoint)
+-- move the waypoint
+runInstruction2 (ship, (wx, wy)) (MoveNorth, arg) = (ship, (wx, wy + arg))
+runInstruction2 (ship, (wx, wy)) (MoveSouth, arg) = (ship, (wx, wy - arg))
+runInstruction2 (ship, (wx, wy)) (MoveEast, arg) = (ship, (wx + arg, wy))
+runInstruction2 (ship, (wx, wy)) (MoveWest, arg) = (ship, (wx - arg, wy))
+-- rotate the waypoint about the ship
+runInstruction2 (ship, (wx, wy)) (TurnLeft, 90) = (ship, (wy * (-1), wx))
+runInstruction2 (ship, (wx, wy)) (TurnLeft, n) = runInstruction2 (ship, (wy * (-1), wx)) (TurnLeft, n - 90)
+runInstruction2 (ship, (wx, wy)) (TurnRight, 90) = (ship, (wy, wx * (-1)))
+runInstruction2 (ship, (wx, wy)) (TurnRight, n) = runInstruction2 (ship, (wy, wx * (-1))) (TurnRight, n - 90)
+runInstruction2 ((sx, sy), (wx, wy)) (MoveForward, n) = ((sx + (wx * n), sy + (wy * n)), (wx, wy))
+
+run2 :: (Position, Waypoint) -> [Instruction] -> Position
+run2 (ship, _) [] = ship
+run2 (ship, waypoint) (i:is) = run2 (runInstruction2 (ship, waypoint) i) is
+
+part2 :: [Instruction] -> Int
+part2 is =
+    let (x, y) = run2 ((0, 0), (10, 1)) is
+        in manhattanDistance x y
 
 prob12 :: IO ()
 prob12 = do
@@ -56,4 +113,8 @@ prob12 = do
     instructions <- readData "inputs/prob12.txt"
     print ("num instructions: " ++ show (length instructions))
     print ("first instruction: " ++ show (head instructions))
+    let p1 = part1 instructions :: Int
+    print ("part1 result: " ++ (show p1))
+    let p2 = part2 instructions :: Int
+    print ("part2 result: " ++ (show p2))
 
